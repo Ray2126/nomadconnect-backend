65b4ecf3c4ccc3069edef2e980a27288403fdd0c
Raymond Yang <raymondyang@outlook.co.nz>
2024-06-03 19:33:37 +1200

    Add guard for deploy step

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import mongodb from './mongodb.js';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', async (req, res) => {
+app.get('/', async ( req, res) => {
   const data = await mongodb.db('sample_airbnb').collection('listingsAndReviews').findOne({ _id: '10006546' });
   res.send(JSON.stringify(data));
 });
