10fcb0c62a1e1eb21ff338cb0637e63df692f912
Raymond Yang <raymondyang@outlook.co.nz>
2024-06-03 18:10:36 +1200

    Remove 0.0.0.0 server binding

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
