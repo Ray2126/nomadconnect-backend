ffb28ff6e7123e9b6ac838233a706d9693a633e1
Raymond Yang <raymondyang@outlook.co.nz>
2024-06-03 18:38:21 +1200

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const mongodb = require('./mongodb');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const data = await mongodb.db("sample_airbnb").collection('listingsAndReviews').findOne({ _id: '10006546' });
+  res.send(JSON.stringify(data));
 })
 
 app.listen(port, () => {
