7c87006d19be865d01b686288409be877877355f
Raymond Yang <raymondyang@outlook.co.nz>
2024-07-18 22:09:46 +1200

    Change get user route
    The get user route should only be called by the user

--- src/users/routes/__tests__/getUser.route.test.js
+++ src/users/routes/__tests__/getUser.route.test.js
@@ -6,7 +6,7 @@ describe('Get User endpoint', () => {
   it('should return the user when given correct ID', async () => {
     const { userId, jwt } = await mockSignUp();
     const res = await request(app)
-      .get(`/api/users/${userId}`)
+      .get('/api/users')
       .set('Cookie', [`token=${jwt}`])
       .send();
 
@@ -15,21 +15,6 @@ describe('Get User endpoint', () => {
       id: userId,
       createdAt: expect.anything(),
       email: 'testing@gmail.com',
-    });
-  });
-
-  it('should throw a 403 USER_NOT_FOUND error when given incorrect ID', async () => {
-    const { jwt } = await mockSignUp();
-    const res = await request(app)
-      .get('/api/users/60d5f8f7e3b3c23d74f58069')
-      .set('Cookie', [`token=${jwt}`])
-      .send();
-
-    expect(res.statusCode).toEqual(403);
-    expect(res.body).toEqual({
-      statusCode: 403,
-      error: 'Forbidden',
-      message: 'USER_NOT_FOUND'
     });
   });
 });
