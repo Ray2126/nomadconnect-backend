7e8732ebd9611c2571dc29f331c288f8254eb727
Raymond Yang <raymond.yang@centrapay.com>
2024-07-25 21:27:17 +1200

    Add profile picture to the update profile API

--- src/users/routes/updateProfile.route.js
+++ src/users/routes/updateProfile.route.js
@@ -4,6 +4,9 @@ import validatePayload from '../../middleware/validatePayload.js';
 import Joi from 'joi';
 import usersCollection from '../mongodb/usersCollection.js';
 
+const TWO_MEGA_BYTES = 2 * 1024 * 1024;
+const PROFILE_PICTURE_SIZE_LIMIT = Math.ceil(TWO_MEGA_BYTES * 1.33); // Approximate calculation for base64 encoding
+
 const schema = Joi.object({
   name: Joi.string().optional(),
   bio: Joi.string().optional(),
@@ -13,7 +16,11 @@ const schema = Joi.object({
   interests: Joi.array().optional(),
   occupation: Joi.string().optional(),
   socialLinks: Joi.array().optional(),
-  //TODO profilePicture: Joi.string().optional(),
+  profilePicture: Joi
+    .string()
+    .pattern(/^data:image\/(jpeg|jpg|png|gif|bmp|webp|svg);base64,[A-Za-z0-9+/=]+$/)
+    .max(PROFILE_PICTURE_SIZE_LIMIT)
+    .optional(),
 }).min(1);
 
 async function handleUpdateProfile(req, res) {
